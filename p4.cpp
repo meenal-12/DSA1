307fbf3ca7910f9ce3bdabd4508bbf9499723116
meenal-12 <60273758+meenal-12@users.noreply.github.com>
2021-11-25 23:48:21 +0530

    Add files via upload

---
+++ p4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void traversal(Node *head)
+{
+
+    while (head != NULL)
+    {
+        cout << head->data << endl;
+        head = head->next;
+    }
+}
+
+int main(){
+   Node* head = new Node(10);
+   Node* temp1 = new Node(20);
+   Node* temp2 = new Node(30);
+   Node* temp3 = new Node(40);
+   head->next = temp1;
+   temp1->next = temp2;
+   temp2->next = temp3; 
+   traversal(head);
+}
