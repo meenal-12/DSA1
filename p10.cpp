7459d676f1299b9a54d514c89f5dc757fbb51fe2
meenal-12 <60273758+meenal-12@users.noreply.github.com>
2021-11-26 02:51:22 +0530

    Add files via upload

---
+++ p10.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* insertAtBegin(Node* head,int x){
+    Node* curr = head;
+    Node* temp = new Node(x);
+    if(head==NULL){
+        head = temp;
+        return head;
+    }
+    else{
+        temp->next = curr->next;
+        curr = temp;
+        return curr;
+    }
+}
+
+void printList(Node *head)
+{
+
+    while (head != NULL)
+    {
+        cout << head->data << endl;
+        head = head->next;
+    }
+}
+
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    printList(head);
+    head = insertAtBegin(head,99);
+    cout<<"list after inserting at first"<<endl;
+    printList(head);
+}
