246a5f27df8396c0dc58dd93ea4560b28d2299f7
meenal <meenalchourasiya@gmail.com>
2021-11-27 02:46:29 +0530

    deletehead

---
+++ deletehead.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+   int data;
+   Node* next;
+   Node* previous;
+   Node(int x){
+       data = x;
+       next = NULL;
+       previous = NULL;
+    }
+
+};
+
+Node* insertatEnd(Node* head,int x){
+    Node* curr = head;
+    Node* temp = new Node(x);
+    if(head == NULL)
+     {
+       return temp;
+     }
+    
+    while(head->next!=NULL){
+        head = head->next;
+    }
+        temp->previous = head;
+        head->next = temp;
+        return curr;
+}
+
+Node* deletehead(Node* head){
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+      return head;
+}
+
+
+
+void printList(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<endl;
+        head=head->next;
+    }
+
+}
+
+int main(){
+       Node* head = NULL;
+       head = insertatEnd(head,10);
+       head = insertatEnd(head,20);
+       head = insertatEnd(head,30);
+       cout<<"before deleting"<<endl;
+       printList(head);
+       cout<<"after deleting first node"<<endl;
+       head = deletehead(head);
+       printList(head);
+    
+}
