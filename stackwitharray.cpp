935aa56baa4d8a95a3ee202344f388debed6bdc9
meenal <meenalchourasiya@gmail.com>
2021-11-27 12:11:12 +0530

    stackwitharray

--- stackwitharray.cpp
+++ stackwitharray.cpp
@@ -16,6 +16,21 @@ int insertAtTop(int* a,int curr,int size,int val){
         }
 }
 
+int insertAtBottom(int* a,int curr,int size,int val){
+        
+        const int start = (curr == size - 1) ? curr : curr +1;
+        
+        for(int i= start ;i>0;i--)
+           {
+             a[i+1] = a[i];
+           }
+        a[0] = val;
+
+        return start;
+      
+
+}
+
 
 void printStack(int* a,int curr){
     while(curr>-1){
@@ -33,6 +48,10 @@ int main(){
     curr = insertAtTop(a,curr,size,20);
     curr = insertAtTop(a,curr,size,30);
     curr = insertAtTop(a,curr,size,40);
+    cout<<"before inserting at bottom"<<endl;
+    printStack(a,curr);
+    curr = insertAtTop(a,curr,size,50);
+    cout<<"after inserting at bottom"<<endl;
     printStack(a,curr);
 
 }
